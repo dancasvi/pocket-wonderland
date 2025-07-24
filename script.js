0a9ded69b8b623b6509b511345366d11a07bcdab
DanielCastagnaVieira <daniel.vieira@helpper.com.br>
2025-07-23 22:06:52 -0300

    fix: musica background so inicia apos dar play

--- script.js
+++ script.js
@@ -61,14 +61,14 @@ window.addEventListener('DOMContentLoaded', () => {
       e.stopPropagation();
       e.preventDefault();
       console.log('Menu clicado:', label);
-      let gameSound = new Audio(`assets/sounds/menu.mp3`);
-      gameSound.currentTime = 0;
-      gameSound.volume = 0.3;
-      gameSound.play().catch(err => console.warn('Som bloqueado:', err));
-      window.gameSound = gameSound;
 
       switch (label) {
         case 'Play':
+          let gameSound = new Audio(`assets/sounds/menu.mp3`);
+          gameSound.currentTime = 0;
+          gameSound.volume = 0.3;
+          gameSound.play().catch(err => console.warn('Som bloqueado:', err));
+          window.gameSound = gameSound;
           loadComponent('components/phase-component/phase-component.html');
           break;
         case 'Choose Character':
